11998c318ecb853f33ebc66ba2bd08595a443fcf
Adnan <adnanali23189@gmail.com>
2025-02-17 19:44:11 +0530

    deploy check

--- app/Personalized/page.tsx
+++ app/Personalized/page.tsx
@@ -58,6 +58,7 @@ function Page() {
             className="bg-indigo-500 text-white px-4 py-2 rounded-md shadow hover:bg-indigo-600 transition ml-auto"
           >
             Next
+            
           </button>
         )}
       </div>
